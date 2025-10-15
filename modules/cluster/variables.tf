10c37c106f6b5bd85e224b9a4873626067ec80f6
unknown <jhonathan-arraes@hotmail.com>
2025-10-15 20:48:10 -0300

    adicionado security group e criação do EKS cluster e IAM role

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -7,3 +7,13 @@ variable "tags" {
   type        = map(any)
   description = "tags para adicionar nos recursos AWS"
 }
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "subnet para criar o EKS cluster AZ 1a"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "subnet para criar o EKS cluster AZ 1b"
+}
